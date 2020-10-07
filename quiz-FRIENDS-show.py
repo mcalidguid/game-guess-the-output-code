352689ec482a398e41f1639baca89879dcf9a245
mcalidguid <marifelcalidguid29@gmail.com>
2020-10-07 21:37:27 +0800

    modified the code

--- quiz-FRIENDS-show.py
+++ quiz-FRIENDS-show.py
@@ -6,16 +6,16 @@ score = 0
 with open("questions.json", 'r+') as f:
     question_list = json.load(f)
     for i in range(10):
-        number_of_questions = len(question_list)
-        question = random.randint(0, number_of_questions - 1)
-        print(f'\nQ{i + 1} {question_list[question]["question"]}\n')
+        total_questions = len(question_list)
+        question = random.randint(0, total_questions - 1)
+        print(f'\nQ{i + 1} {question_list[question]["question"]}')
         for option in question_list[question]["options"]:
             print(option)
-        answer = input("\nEnter your answer: ")
+        answer = input("Enter your answer: ")
         if question_list[question]["answer"][0] == answer[0].upper():
-            print("\nYou are correct")
+            print("You are correct")
             score += 1
         else:
-            print("\nYou are incorrect")
+            print("You are incorrect")
         del question_list[question]
-    print(f'\nFINAL SCORE: {score}')
+    print(f'FINAL SCORE: {score}')
