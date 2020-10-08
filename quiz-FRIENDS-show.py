a08fbb787280ba74c097f08573cf6bdddc024ffa
mcalidguid <marifelcalidguid29@gmail.com>
2020-10-08 23:32:11 +0800

    modified the code

--- quiz-FRIENDS-show.py
+++ quiz-FRIENDS-show.py
@@ -1,9 +1,35 @@
 import json
 import random
 
-print("~#--------------#~  FRIENDS  ~#--------------#~")
+print("""-------------------------------------------------------------------
+~#--------------#~  Are You A True FRIENDS Fan?  ~#--------------#~
+-------------------------------------------------------------------
+It’s been 25 years since the show aired and we’re still watching it. 
+Either way, do you know enough about Friends? Let’s find out!
+  Choose level of difficulty:
+    Enter \'1\' for Easy \"I think so. I have watched FRIENDS!\"
+    Enter \'2\' for Medium \"You bet! I know enough about FRIENDS.\"
+    Enter \'3\' for Hard \"Ahaa! I'm a true FRIENDS superfan!\"
+    Enter \'4\' for Quit \"Dang! I'm scared!\"
+-------------------------------------------------------------------""")
+user_input = int(input(">>>: "))
+    if user_input == 1:
+        number_of_question = 5
+
+    elif user_input == 2:
+        number_of_question = 10
+
+    elif user_input == 3:
+        number_of_question = 20
+
+    elif user_input == 4:
+        exit()
+
+    else:
+        print("Invalid input.")
+
+
 score = 0
-number_of_question = 5
 with open("questions.json", 'r') as f:
     question_list = json.load(f)
     for i in range(number_of_question):
