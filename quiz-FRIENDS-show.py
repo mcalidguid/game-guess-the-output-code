8f3517bb580c384ee751e3ea9266d014155c30fd
mcalidguid <marifelcalidguid29@gmail.com>
2020-10-08 23:10:09 +0800

    modified the code

--- quiz-FRIENDS-show.py
+++ quiz-FRIENDS-show.py
@@ -5,7 +5,6 @@ print("~#--------------#~  FRIENDS  ~#--------------#~")
 score = 0
 number_of_question = 5
 with open("questions.json", 'r') as f:
-    # laman  ng json file
     question_list = json.load(f)
     for i in range(number_of_question):
         total_questions = len(question_list)
@@ -16,9 +15,10 @@ with open("questions.json", 'r') as f:
             print(option)
         answer = input(">>> Enter your answer: ")
         if question_list[question]["answer"][0] == answer[0].upper():
-            print("You were right!")
+            print("＼(*ﾟ∀ﾟ*)／ You were right!")
             score += 1
         else:
-            print("Close but not quite...")
+            # print("Close but not quite... ヾ(￣◇￣)ノ")
+            print("ヾ(￣◇￣)ノ Hmm close but not quite... ")
         del question_list[question]
     print("\nYour score is", score)
